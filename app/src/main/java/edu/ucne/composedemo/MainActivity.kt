68e6444cf31f27a82d1aa40d915e29e835ade8ad
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-09-16 12:40:05 -0400

    Navegacion entre pantallas Jugador y Partidas

--- app/src/main/java/edu/ucne/composedemo/MainActivity.kt
+++ app/src/main/java/edu/ucne/composedemo/MainActivity.kt
@@ -1,48 +1,65 @@
-
 package edu.ucne.composedemo
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.composedemo.Presentation.Navigation.Screen
 import edu.ucne.composedemo.ui.theme.TicTacToePlayersTheme
+import edu.ucne.composedemo.Presentation.Jugador.JugadorListScreen
+import edu.ucne.composedemo.Presentation.Jugador.JugadorScreen
+import edu.ucne.composedemo.Presentation.Partidas.PartidaListScreen
+import edu.ucne.composedemo.Presentation.Partidas.PartidaFormScreen
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TicTacToePlayersTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val nav = rememberNavController()
+
+                NavHost(navController = nav, startDestination = Screen.List) {
+
+                    composable<Screen.List> {
+                        JugadorListScreen(
+                            goToJugadores = { id -> nav.navigate(Screen.Jugador(id)) },
+                            createJugador = { nav.navigate(Screen.Jugador(null)) }
+                        )
+                    }
+
+                    composable<Screen.Jugador> { backStackEntry ->
+                        val args = backStackEntry.toRoute<Screen.Jugador>()
+                        JugadorScreen(
+                            jugadorId = args.Id,
+                            goback = { nav.popBackStack() },
+                            id = args.Id
+                        )
+                    }
+
+                    
+                    composable<Screen.Partidas> {
+                        PartidaListScreen(
+                            onNueva = { nav.navigate(Screen.Partida(null)) },
+                            onEditar = { id -> nav.navigate(Screen.Partida(id)) }
+                        )
+                    }
+
+                    composable<Screen.Partida> { backStackEntry ->
+                        val args = backStackEntry.toRoute<Screen.Partida>()
+                        PartidaFormScreen(
+                            partidaId = args.id,
+                            onBack = { nav.popBackStack() }
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TicTacToePlayersTheme {
-        Greeting("Android")
     }
 }
