4c74f8fe227094a5dcbc213764f20faeb5dd4132
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-10-20 19:19:02 -0400

    Refactorización de Navegación y MainActivity

--- app/src/main/java/edu/ucne/composedemo/MainActivity.kt
+++ app/src/main/java/edu/ucne/composedemo/MainActivity.kt
@@ -1,62 +1,52 @@
-package edu.ucne.composedemo
+package edu.ucne.jugadorestictactoe
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
-import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.composedemo.Presentation.Navigation.Screen
-import edu.ucne.composedemo.ui.theme.TicTacToePlayersTheme
-import edu.ucne.composedemo.Presentation.Jugador.JugadorListScreen
-import edu.ucne.composedemo.Presentation.Jugador.JugadorScreen
-import edu.ucne.composedemo.Presentation.Partidas.PartidaListScreen
-import edu.ucne.composedemo.Presentation.Partidas.PartidaFormScreen
+import androidx.navigation.navArgument
+import edu.ucne.jugadorestictactoe.Presentation.Jugador.JugadorListScreen
+import edu.ucne.jugadorestictactoe.Presentation.Jugador.JugadorScreen
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TicTacToePlayersTheme {
-                val nav = rememberNavController()
+            MaterialTheme {
+                Surface {
+                    val navController = rememberNavController()
 
-                NavHost(navController = nav, startDestination = Screen.List) {
-
-                    composable<Screen.List> {
-                        JugadorListScreen(
-                            goToJugadores = { id -> nav.navigate(Screen.Jugador(id)) },
-                            createJugador = { nav.navigate(Screen.Jugador(null)) }
-                        )
-                    }
-
-                    composable<Screen.Jugador> { backStackEntry ->
-                        val args = backStackEntry.toRoute<Screen.Jugador>()
-                        JugadorScreen(
-                            jugadorId = args.Id,
-                            goback = { nav.popBackStack() },
-                            id = args.Id
-                        )
-                    }
-
-                    
-                    composable<Screen.Partidas> {
-                        PartidaListScreen(
-                            onNueva = { nav.navigate(Screen.Partida(null)) },
-                            onEditar = { id -> nav.navigate(Screen.Partida(id)) }
-                        )
-                    }
-
-                    composable<Screen.Partida> { backStackEntry ->
-                        val args = backStackEntry.toRoute<Screen.Partida>()
-                        PartidaFormScreen(
-                            partidaId = args.id,
-                            onBack = { nav.popBackStack() }
-                        )
+                    NavHost(
+                        navController = navController,
+                        startDestination = "jugadores"
+                    ) {
+                        composable("jugadores") {
+                            JugadorListScreen(
+                                goToJugadores = { id ->
+                                    navController.navigate("jugador/$id")
+                                },
+                                createJugador = {
+                                    navController.navigate("jugador/0")
+                                }
+                            )
+                        }
+                        composable(
+                            route = "jugador/{jugadorId}",
+                            arguments = listOf(
+                                navArgument("jugadorId") { type = NavType.IntType }
+                            )
+                        ) { backStackEntry ->
+                            val jugadorId = backStackEntry.arguments?.getInt("jugadorId")
+                            JugadorScreen(
+                                jugadorId = jugadorId,
+                                goback = { navController.popBackStack() }
+                            )
+                        }
                     }
                 }
             }
