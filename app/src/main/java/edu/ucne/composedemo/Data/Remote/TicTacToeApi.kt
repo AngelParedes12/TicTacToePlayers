6c054aca314fcb503aa93bb7a2e30111760a0bf8
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-10-21 01:05:16 -0400

    Corrección de rutas de Dto y cambios en la API agregando Partida

--- app/src/main/java/edu/ucne/composedemo/Data/Remote/TicTacToeApi.kt
+++ app/src/main/java/edu/ucne/composedemo/Data/Remote/TicTacToeApi.kt
@@ -2,8 +2,8 @@ package edu.ucne.composedemo.Data.Remote
 
 import edu.ucne.composedemo.Data.Remote.dto.MovimientoDto
 import edu.ucne.composedemo.Data.Remote.dto.MovimientoPostDto
-import edu.ucne.composedemo.Data.Remote.Dto.PartidaDto
-import edu.ucne.composedemo.Data.Remote.Dto.PartidaPostDto
+import edu.ucne.composedemo.Data.Remote.dto.PartidaDto
+import edu.ucne.composedemo.Data.Remote.dto.PartidaPostDto
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -11,15 +11,18 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface TicTacToeApi {
+
     @GET("api/Movimientos/{partidaId}")
     suspend fun getMovimientos(@Path("partidaId") partidaId: Int): List<MovimientoDto>
 
     @POST("api/Movimientos")
     suspend fun postMovimiento(@Body body: MovimientoPostDto): Response<Unit>
 
+    @GET("api/Partidas")
+    suspend fun getPartidas(): List<PartidaDto>
+
     @GET("api/Partidas/{id}")
     suspend fun getPartida(@Path("id") id: Int): PartidaDto
-
     @POST("api/Partidas")
-    suspend fun crearPartida(@Body body: PartidaPostDto): PartidaDto
+    suspend fun crearPartida(@Body body: PartidaPostDto): Response<Unit>
 }
