73e60f7365ed79d17419a9ce4236fe67120b2695
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-10-20 20:53:42 -0400

    Configuración de Retrofit y repositorios para la API

--- app/src/main/java/edu/ucne/composedemo/Di/AppModule.kt
+++ app/src/main/java/edu/ucne/composedemo/Di/AppModule.kt
@@ -1,54 +1,57 @@
 package edu.ucne.composedemo.Di
 
-import android.content.Context
-import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
-import edu.ucne.composedemo.Data.Local.Database.JugadorDb
-import edu.ucne.composedemo.Data.Local.Dao.JugadorDao
-import edu.ucne.composedemo.Data.Repository.JugadorRepositorylmpl
-import edu.ucne.composedemo.Domain.Repository.JugadorRepository
-import edu.ucne.composedemo.Domain.useCase.EliminarJugadorUseCase
-import edu.ucne.composedemo.Domain.useCase.GuardarJugadorUseCase
-import edu.ucne.composedemo.Domain.useCase.JugadorUseCases
-import edu.ucne.composedemo.Domain.useCase.ObtenerJugadorUseCase
-import edu.ucne.composedemo.Domain.useCase.ObtenerJugadoresUseCase
-import edu.ucne.composedemo.Domain.useCase.ValidarJugadorUseCase
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import edu.ucne.composedemo.Data.Remote.TicTacToeApi
+import edu.ucne.composedemo.Domain.Repository.MovimientosRepository
+import edu.ucne.composedemo.Data.Repository.MovimientosRepositoryImpl
+import edu.ucne.composedemo.Domain.Repository.PartidasRepository
+import edu.ucne.composedemo.Data.Repository.PartidasRepositoryImpl
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    @Singleton
+    fun provideOkHttp(): OkHttpClient {
+        val log = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient.Builder().addInterceptor(log).build()
+    }
 
     @Provides
     @Singleton
-    fun provideDb(@ApplicationContext appContext: Context): JugadorDb =
-        Room.databaseBuilder(appContext, JugadorDb::class.java, "JugadorDb")
-            .fallbackToDestructiveMigration()
+    fun provideRetrofit(client: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://gestionhuacalesapi.azurewebsites.net/")
+            .client(client)
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
 
     @Provides
-    fun provideJugadorDao(db: JugadorDb): JugadorDao =
-        db.JugadorDao()
+    @Singleton
+    fun provideApi(retrofit: Retrofit): TicTacToeApi =
+        retrofit.create(TicTacToeApi::class.java)
 
     @Provides
     @Singleton
-    fun provideJugadorRepository(dao: JugadorDao): JugadorRepository =
-        JugadorRepositorylmpl(dao)
+    fun provideMovimientosRepo(api: TicTacToeApi): MovimientosRepository =
+        MovimientosRepositoryImpl(api)
+
 
     @Provides
-    fun provideJugadorUseCases(repository: JugadorRepository): JugadorUseCases {
-        val validar = ValidarJugadorUseCase(repository)
-        val obtenerTodos = ObtenerJugadoresUseCase(repository)
-        return JugadorUseCases(
-            validarJugador = validar,
-            guardarJugador = GuardarJugadorUseCase(repository, validar),
-            eliminarJugador = EliminarJugadorUseCase(repository),
-            obtenerJugador = ObtenerJugadorUseCase(repository),
-            obtenerJugadores = obtenerTodos
-        )
-    }
+    @Singleton
+    fun providePartidasRepository(api: TicTacToeApi): PartidasRepository =
+        PartidasRepositoryImpl(api)
+
 }
