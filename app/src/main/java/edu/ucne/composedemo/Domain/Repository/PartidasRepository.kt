a1c9c27a3b253f1911a16e50beeb6faa2bf3e149
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-10-21 01:06:10 -0400

    Se agrega función `list` al `PartidasRepository`

--- app/src/main/java/edu/ucne/composedemo/Domain/Repository/PartidasRepository.kt
+++ app/src/main/java/edu/ucne/composedemo/Domain/Repository/PartidasRepository.kt
@@ -1,9 +1,10 @@
 package edu.ucne.composedemo.Domain.Repository
 
-import edu.ucne.composedemo.Data.Remote.Dto.PartidaDto
+import edu.ucne.composedemo.Data.Remote.dto.PartidaDto
 
 interface PartidasRepository {
     suspend fun exists(id: Int): Boolean
     suspend fun create(jugador1Id: Int, jugador2Id: Int): PartidaDto
     suspend fun get(id: Int): PartidaDto
+    suspend fun list(): List<PartidaDto>
 }
