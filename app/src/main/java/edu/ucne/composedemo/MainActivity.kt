0547f75c0f717b7472a2027c813113f763f9b8f4
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-10-21 01:08:06 -0400

    Configuración de navegación para Partidas y Juego
    Se implementa la navegación entre la lista de partidas (`PartidasListScreen`) y la pantalla de juego (`JugadorScreen`) utilizando `NavHost`. Se define el paso de argumentos para el ID de la partida.

--- app/src/main/java/edu/ucne/composedemo/MainActivity.kt
+++ app/src/main/java/edu/ucne/composedemo/MainActivity.kt
@@ -7,8 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.composedemo.Presentation.Jugador.JugadorScreen
+import edu.ucne.composedemo.Presentation.Partida.PartidasListScreen
 import edu.ucne.composedemo.ui.theme.TicTacToePlayersTheme
 
 @AndroidEntryPoint
@@ -19,7 +25,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             TicTacToePlayersTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    JugadorScreen()
+                    val nav = rememberNavController()
+                    NavHost(navController = nav, startDestination = "partidas") {
+                        composable("partidas") {
+                            PartidasListScreen(goToPartida = { id -> nav.navigate("juego/$id") })
+                        }
+                        composable(
+                            route = "juego/{id}",
+                            arguments = listOf(navArgument("id") { type = NavType.IntType })
+                        ) { backStack ->
+                            val id = backStack.arguments?.getInt("id")
+                            JugadorScreen(partidaIdArg = id)
+                        }
+                    }
                 }
             }
         }
