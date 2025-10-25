c686739923984664cb0c34ed5fb1fb126829f662
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-10-25 13:27:28 -0400

    Refactorización del JugadorRepository

--- app/src/main/java/edu/ucne/composedemo/Domain/Repository/JugadorRepository.kt
+++ app/src/main/java/edu/ucne/composedemo/Domain/Repository/JugadorRepository.kt
@@ -1,12 +1,14 @@
 package edu.ucne.composedemo.Domain.Repository
 
 import edu.ucne.composedemo.Domain.Model.Jugador
+import edu.ucne.composedemo.Util.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface JugadorRepository {
-    suspend fun save(jugador: Jugador): Boolean
-    suspend fun find(id: Int): Jugador?
-    suspend fun delete(jugador: Jugador)
-    suspend fun getAll(): List<Jugador>
-    fun getAllFlow(): Flow<List<Jugador>>
+    fun observe(): Flow<List<Jugador>>
+    suspend fun guardarLocal(nombres: String, apellidos: String?, telefono: String?, email: String?, partidas: Int): Resource<Jugador>
+    suspend fun postPendientes(): Resource<Unit>
+    suspend fun syncFull(): Resource<Unit>
+    suspend fun obtener(id: String): Jugador?
+    suspend fun eliminar(id: String): Resource<Unit>
 }
