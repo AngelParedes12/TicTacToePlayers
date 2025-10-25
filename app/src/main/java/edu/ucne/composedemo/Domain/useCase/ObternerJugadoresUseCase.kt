4229badb01c193eeda6ab528c0a6e5173447f18e
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-10-25 13:28:23 -0400

    Refactorización del caso de uso ObtenerJugadores
    Se simplifica la clase `ObtenerJugadoresUseCase` para usar una expresión concisa. El método `invoke` ahora llama directamente a `repo.observe()` en lugar de a `repository.getAllFlow()`. También se han eliminado importaciones no utilizadas y se ha renombrado la variable del repositorio.

--- app/src/main/java/edu/ucne/composedemo/Domain/useCase/ObternerJugadoresUseCase.kt
+++ app/src/main/java/edu/ucne/composedemo/Domain/useCase/ObternerJugadoresUseCase.kt
@@ -1,13 +1,7 @@
 package edu.ucne.composedemo.Domain.useCase
 
-import edu.ucne.composedemo.Domain.Model.Jugador
 import edu.ucne.composedemo.Domain.Repository.JugadorRepository
-import kotlinx.coroutines.flow.Flow
 
-class ObtenerJugadoresUseCase(
-    private val repository: JugadorRepository
-) {
-    operator fun invoke(): Flow<List<Jugador>> {
-        return repository.getAllFlow()
-    }
+class ObtenerJugadoresUseCase(private val repo: JugadorRepository) {
+    operator fun invoke() = repo.observe()
 }
