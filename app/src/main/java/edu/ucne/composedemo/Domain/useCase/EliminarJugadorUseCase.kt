3f45d42505d8ec9ac561e25baab0f6646b411c10
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-10-25 13:29:26 -0400

    Refactorización de EliminarJugadorUseCase

--- app/src/main/java/edu/ucne/composedemo/Domain/useCase/EliminarJugadorUseCase.kt
+++ app/src/main/java/edu/ucne/composedemo/Domain/useCase/EliminarJugadorUseCase.kt
@@ -1,12 +1,7 @@
 package edu.ucne.composedemo.Domain.useCase
 
 import edu.ucne.composedemo.Domain.Repository.JugadorRepository
-import edu.ucne.composedemo.Domain.Model.Jugador
 
-class EliminarJugadorUseCase(
-    private val repository: JugadorRepository
-) {
-    suspend operator fun invoke(jugador: Jugador) {
-        repository.delete(jugador)
-    }
+class EliminarJugadorUseCase(private val repo: JugadorRepository) {
+    suspend operator fun invoke(id: String) = repo.eliminar(id)
 }
