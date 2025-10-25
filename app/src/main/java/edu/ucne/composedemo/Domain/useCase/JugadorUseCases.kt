fe25011c944829707cbbd65e10feca1f0e47f93b
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-10-25 13:28:48 -0400

    Actualización de los casos de uso de Jugador
    Se refactoriza la clase `JugadorUseCases` para incluir nuevos casos de uso relacionados con la sincronización de datos y operaciones locales:

--- app/src/main/java/edu/ucne/composedemo/Domain/useCase/JugadorUseCases.kt
+++ app/src/main/java/edu/ucne/composedemo/Domain/useCase/JugadorUseCases.kt
@@ -1,8 +1,11 @@
 package edu.ucne.composedemo.Domain.useCase
+
 data class JugadorUseCases(
-    val validarJugador: ValidarJugadorUseCase,
-    val guardarJugador: GuardarJugadorUseCase,
-    val eliminarJugador: EliminarJugadorUseCase,
-    val obtenerJugador: ObtenerJugadorUseCase,
-    val obtenerJugadores: ObtenerJugadoresUseCase
+    val guardarLocal: GuardarJugadorLocalUseCase,
+    val obtenerTodos: ObtenerJugadoresUseCase,
+    val obtener: ObtenerJugadorUseCase,
+    val eliminar: EliminarJugadorUseCase,
+    val validar: ValidarJugadorUseCase,
+    val postPendientes: PostPendientesUseCase,
+    val syncFull: SyncJugadoresUseCase
 )
