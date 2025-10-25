6b03c9f5536c7d1ca09b09f6de25c6c2d8d5e504
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-10-25 13:32:13 -0400

    Refactor: Aplicación del color de fondo del tema

--- app/src/main/java/edu/ucne/composedemo/MainActivity.kt
+++ app/src/main/java/edu/ucne/composedemo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.composedemo.Presentation.Jugador.JugadorScreen
@@ -18,7 +19,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TicTacToePlayersTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     JugadorScreen()
                 }
             }
